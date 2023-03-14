2e0fbfa6938668667f0cc659bacaba5d040ed7f3
Kh. Khalid Hosen <127880467+uxkhalid@users.noreply.github.com>
2023-03-15 02:00:59 +0600

    Update cont.php

--- assets/php/cont.php
+++ assets/php/cont.php
@@ -1,15 +1,23 @@
 <?php
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
+if(isset($_POST['submit'])) {
+    $to = "your-email-address@example.com"; // your email address
+    $subject = "New contact form submission";
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $headers = "From: " . $name . " <" . $email . ">\r\n";
+    $headers .= "Reply-To: " . $email . "\r\n";
+    $headers .= "Content-Type: text/html\r\n";
 
-$to = 'khaliduxui@gmail.com';
-$subject = 'New message from website contact form';
-$body = "Name: $name\nEmail: $email\nMessage: $message";
+    $body = "<h2>New contact form submission</h2>
+             <p><strong>Name:</strong> {$name}</p>
+             <p><strong>Email:</strong> {$email}</p>
+             <p><strong>Message:</strong> {$message}</p>";
 
-if (mail($to, $subject, $body)) {
-    echo 'Message sent successfully.';
-} else {
-    echo 'Message sending failed.';
+    if(mail($to, $subject, $body, $headers)) {
+        echo "<p>Thank you for contacting us. We will be in touch soon.</p>";
+    } else {
+        echo "<p>There was an error sending your message. Please try again later.</p>";
+    }
 }
 ?>
